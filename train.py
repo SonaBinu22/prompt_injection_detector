e63cb09f5040674df51c2e0d531f9b298246a164
SonaBinu22 <sonabinu22@gmail.com>
2026-02-20 15:10:09 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,20 +1,21 @@
 import pandas as pd
+import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-import pickle
-from preprocess import clean_text
 
-df = pd.read_csv("../data/dataset.csv")
+print("Loading dataset...")
+df = pd.read_csv("../data/final_dataset.csv")
 
-df["prompt"] = df["prompt"].apply(clean_text)
+X = df["text"]
+y = df["label"]
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(df["prompt"])
+vectorizer = TfidfVectorizer(ngram_range=(1,2), max_features=5000)
+X_vec = vectorizer.fit_transform(X)
 
-model = LogisticRegression()
-model.fit(X, df["label"])
+model = LogisticRegression(max_iter=200)
+model.fit(X_vec, y)
 
 pickle.dump(model, open("model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
-print("Model trained and saved!")
+print("Model and vectorizer saved successfully!")
